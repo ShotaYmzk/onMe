69c28d0bdb4f79f68c802020f355655788532ac8
ShotaYmzk <obi.wan.kenobi.t_t@icloud.com>
2025-09-22 16:48:36 +0900

    Initial Commit

---
+++ onMe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  onMe
+//
+//  Created by 山﨑彰太 on 2025/09/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ onMe/onMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  onMeApp.swift
+//  onMe
+//
+//  Created by 山﨑彰太 on 2025/09/22.
+//
+
+import SwiftUI
+
+@main
+struct onMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
